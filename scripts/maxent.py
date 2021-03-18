2084f854acace4b92419f7d51c147b985e835fc9
Josef Kaufmann <josefkaufma@gmail.com>
2021-03-18 17:43:53 +0100

    remove unnecessary stuff

--- scripts/maxent.py
+++ scripts/maxent.py
@@ -1,20 +1,10 @@
 import sys, os
-# import numpy as np
-# import h5py
-# import matplotlib.pyplot as plt
-# import scipy.interpolate as interp
 from PyQt5 import QtWidgets
 
 file_dir = os.path.dirname(os.path.abspath(__file__))
 package_dir = '/'.join(file_dir.split('/')[:-1])
 sys.path.insert(0, package_dir)
-
-# import ana_cont.continuation as cont
-# from gui.maxent_ui import Ui_MainWindow
 from gui.maxent_backend import MainWindow
-
-
-
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
