d2102fbf5fd75525ced8a0ebcd763357ec3522e9
Josef Kaufmann <josefkaufma@gmail.com>
2021-03-23 14:10:33 +0100

    clean up setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,22 @@
 from Cython.Distutils import build_ext
-from setuptools import setup
 from distutils.core import setup, Extension
 import numpy
 from Cython.Build import cythonize
 
 cythonize("ana_cont/pade.pyx")
-setup(name = 'ana_cont',
-      version = '0.8',
-      description = 'Analytic continuation package',
-      author = 'Josef Kaufmann',
-      author_email = 'josefkaufma@gmail.com',
-      url = 'https://github.com/josefkaufmann/ana_cont',
-      packages = ['ana_cont'],
-      package_dir = {'ana_cont':'ana_cont/'},
-      cmdclass = {'build_ext': build_ext},
-      ext_modules = [Extension('ana_cont.pade', \
-                        sources = ['ana_cont/pade.c'], \
-                        libraries=['m'], \
-                        include_dirs=[numpy.get_include()])],
+setup(name='ana_cont',
+      version='0.8',
+      description='Analytic continuation package',
+      author='Josef Kaufmann',
+      author_email='josefkaufma@gmail.com',
+      url='https://github.com/josefkaufmann/ana_cont',
+      packages=['ana_cont', 'gui'],
+      package_dir={'ana_cont': 'ana_cont/', 'gui': 'gui/'},
+      cmdclass={'build_ext': build_ext},
+      ext_modules=[Extension('ana_cont.pade',
+                             sources=['ana_cont/pade.c'],
+                             libraries=['m'],
+                             include_dirs=[numpy.get_include()])],
       include_package_data=True,
       zip_safe=False,
       setup_requires=['Cython'],
