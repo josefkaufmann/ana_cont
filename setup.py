1398ab119297bd4d02234d9265f5449249e75102
Josef Kaufmann <josefkaufma@gmail.com>
2022-01-02 18:03:12 +0100

    change version number to 1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from Cython.Build import cythonize
 
 cythonize("ana_cont/pade.pyx")
 setup(name='ana_cont',
-      version='0.8',
+      version='1.0',
       description='Analytic continuation package',
       author='Josef Kaufmann',
       author_email='josefkaufma@gmail.com',
